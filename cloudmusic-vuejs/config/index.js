b17e1d8950dd77d45517086acdbaa8b6007d3b98
zhongjunhaoz <2236241424@qq.com>
2020-03-15 18:21:26 +0800

    部署

--- cloudmusic-vuejs/config/index.js
+++ cloudmusic-vuejs/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'http://zhongjunhaoz.gitee.io/cloudmusic-vuejs',
     proxyTable: {},
 
     // Various Dev Server settings
