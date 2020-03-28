84c34a0b623187df123c4a2d8e379a849207b795
zhongjunhaoz <2236241424@qq.com>
2020-03-28 12:16:43 +0800

    node跨域解决

--- cloudmusic-vuejs/config/index.js
+++ cloudmusic-vuejs/config/index.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/api': {
+        target: 'http://localhost:3000',// 将 URL 中带有 /api 的请求代理到本地的 3000 端口的服务上
+        changeOrigin: true,
+        pathRewrite:{
+          '^api':''
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
